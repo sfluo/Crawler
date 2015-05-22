21d0bda1215417cfff9d9eeb34ffb321dbd8adf5
sfluo <luoshoufu@gmail.com>
2015-05-21 23:47:23 -0400

    samples of books

--- crawl.py
+++ crawl.py
@@ -11,22 +11,29 @@ import urllib2
 import datetime
 import json
 import string
-import re
+import re, os, sys
 import cookielib
+import hashlib
 
 from bs4 import BeautifulSoup
 
 host = 'http://www.amazon.com'
 MaxNumReviews = 15 
 
+# it seems in order to get details of product, cookie is required.
 FakeCookie='skin=noskin; x-wl-uid=1uHgA6QYtOf9VAskEst2YwASvctHz7iD/DW4sDnNew/GMyywt9FUDbwsRnzE39zseg1uFAnaoIOI=; session-token=ULgTXs9bV43xIhlut2kEZI/Le5ZL2aINFopjKFZtgrdqGxxcX/1GSZkYmvCc0+uktkYcJD657Tk9Dsi11JxPnPodmIOYJjBuc4tAAts0ZpR6lbzomtDBPlLh5LnmGAschVmi/T0BOD1Nr2+6qf/WyvMupgeEHH+ya5b4z+aYSY+5jD3LapfzmrqE3jF3ogvn1E+bbPmdR5rrwJkRej25mYbSkOrYqBNHyZNf9TCOrnCNEgOOU/g/JIjb10OaOkAB; __gads=ID=25a2624337c614a3:T=1428607844:S=ALNI_MZfckri_ZZ1-ydzF7K7bsrJYRUofA; __ar_v4=7CUFP6UIQZARTK57SDZKRU%3A20150409%3A3%7CVKZDA7NCVJAINNGOEQVAY3%3A20150409%3A3%7CIQS3HPYPHFHRHEYBEZAXCQ%3A20150409%3A3; ubid-main=188-5471422-6981114; session-id-time=2082787201l; session-id=184-9289097-6318545; csm-hit=1QC1CAANPA2T95A9NVZ4+s-1QC1CAANPA2T95A9NVZ4|1429326231448'
 
+# we want to mimic a web browser
 UserAgent= 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36'
 
 def getPage(url):
+	'''
+		Get a page with customized http header, cookie and user-agent	
+	'''
 
 	tries = 0
 
+	# maximum three failures
 	while tries < 3:
 
 		try:
@@ -50,9 +57,13 @@ def extractReviewer(authorUrl, Author):
 	"""
 		Extract information from reviewer profile
 		Author: ditionary of reviewer info
+		1.	Name
+		2.	Location
+		3.	User Ranking
+		4.	Helpfulness
 	"""
 
-	print(authorUrl)
+	# print(authorUrl)
 	
 	page = getPage(authorUrl)
 	if page is None:
@@ -60,24 +71,28 @@ def extractReviewer(authorUrl, Author):
 
 	soup = BeautifulSoup(page) # put into soup
 	
-	# find the profile
+	# Step 1: locates the profile
 	profile = soup.find('div', class_='profile-info')
 	if profile is None:
 		return
 	
+	# Step 2: extract profile info
 	try:
+		# Task 1: Author Name 
 		name = profile.find('span', class_='profile-display-name break-word')
 		Author['Name'] = name.string.strip()
 	except:
 		return # must have a name 
 
 	try:
+		# Task 2: Location
 		nameblock = profile.find('div', class_='a-row a-spacing-micro')
 		location = nameblock.find('span', class_='a-size-small a-color-secondary')
 		Author['Location'] = location.string.strip()
 	except:
 		Author['Location'] = '' 
 
+	# Task 3: User Ranking
 	for child in profile.children:
 		try: 
 			row = child.find('div', class_='a-row')
@@ -96,6 +111,7 @@ def extractReviewer(authorUrl, Author):
 		except:
 			Author['Ranking'] = ''
 			
+	# Task 4: Helpfulness
 	try:
 		helpful = profile.find('div', class_='a-row customer-helpfulness')
 		rate = helpful.find('span', class_='a-size-large a-text-bold')
@@ -105,9 +121,13 @@ def extractReviewer(authorUrl, Author):
 
 def extractReviews(revUrl, maxNum, reviews):
 	"""
-		revUrl: the URL of all reviews
-		maxNum: the maximum number of reviews we want
-		reviews: the directionary of reviews (return)
+		Extract Reviews from revUrl (recursively)
+		@revUrl: the URL of all reviews
+		@maxNum: the maximum number of reviews we want
+		@reviews: the directionary of reviews (return)
+
+		@return
+			AverageStarRating
 	"""
 	
 	numOfReviews = 0
@@ -120,12 +140,14 @@ def extractReviews(revUrl, maxNum, reviews):
 
 		print(revUrl)
 
+		# Step 1: get the reviews page
 		page = getPage(revUrl)
 		if page is None:
 			break
 
 		soup = BeautifulSoup(page) # put into soup
 		
+		# Task 1: Average Star Rating
 		if reviews['AverageStarRating'] is not '':
 			summary = soup.find('div', class_='a-row averageStarRatingNumerical')
 			if summary is not None:
@@ -139,39 +161,40 @@ def extractReviews(revUrl, maxNum, reviews):
 		if reviewsoup is None:
 			break
 
+		# Task 2: get reviews (list)
 		for review in reviewsoup.children:
 
-			# A review should have a ID
+			# Task 2.0: A review should have a ID
 			if review.get('id') is None:
 				continue;	
 
 			a_review = {}
 			valid = False
 
-			# Helpfulness vote
+			# Task 2.1: Helpfulness vote
 			try:
 				vote = review.find('span', class_='a-size-small a-color-secondary review-votes')
 				hwords = vote.string.split(' ')	
 				hrate = float(hwords[0]) / float(hwords[2])
-				a_review['helpfulnesss'] = hrate
+				a_review['Helpfulness'] = hrate
 			except:
-				a_review['Helpfulnesss'] = ''
+				a_review['Helpfulness'] = ''
 
-			# Rating
+			# Task 2.2: Star Rating
 			try:
 				stars = review.find('span', class_='a-icon-alt')
 				a_review['StarRating'] = stars.string
 			except:
 				a_review['StarRating'] = ''
 
-			# Date
+			# Task 2.3: Review Date
 			date = review.find('span', class_='a-size-base a-color-secondary review-date')
 			if date is not None:
 				a_review['Date'] = date.string
 			else:
 				a_review['Date'] = ''
 
-			# review text
+			# Task 2.4: Review Text
 			try:
 				reviewdata = review.find('div', class_='a-row review-data')
 				text = reviewdata.find('span', class_='a-size-base review-text')
@@ -180,6 +203,7 @@ def extractReviews(revUrl, maxNum, reviews):
 			except:
 				a_review['Text'] = ''
 
+			# Task 2.5: Reviewer 
 			profile = {}
 			reviewer = review.find('a', class_='a-size-base a-link-normal author')
 			if reviewer is not None:
@@ -195,7 +219,7 @@ def extractReviews(revUrl, maxNum, reviews):
 
 			numOfReviews += 1
 
-		# Okay, we had enough, time to leave
+		# Step 3: Okay, we had enough, time to leave
 		if numOfReviews > maxNum:
 			break
 
@@ -217,9 +241,9 @@ def fetchItem(itemurl, record):
 		We are assuming that Amazon use a template for all their products
 		because the extraction heavily depends on the tokens (fix strings)
 
-		Be careful!!! This may vary from products. 
+		Alert: the page might use different template for different categories of products
 	"""
-	print(itemurl)
+	# print(itemurl)
 
 	page = getPage(itemurl)
 	if page is None:
@@ -229,12 +253,12 @@ def fetchItem(itemurl, record):
 
 	# Here, we extract all information we need
 
-	# 0. Time Stamp 
+	# Task 0. Time Stamp 
 	ts = str(datetime.datetime.now())
 	record['Timestamp'] = ts;
 
-	# 1. Sales Rank
-	# 2. Product Category
+	# Task 1. Sales Rank
+	# Task 2. Product Category
 	salesrank = soup.find(id='SalesRank')
 	if salesrank is not None:
 		rank = salesrank.contents[2].strip().split(' ', 1)[0]
@@ -245,8 +269,8 @@ def fetchItem(itemurl, record):
 		record['Salesrank'] = ''
 		record['Category'] = '' 
 
-	# 3. Release time
-	# 5. Price 
+	# Task.3. Release time
+	# Task5. Price 
 	# Note!!! e.g. Kindle eBook (free) does not have a price
 	# Different category of products may have different page layout, different tags
 	# FIXME: So far, the following is only for Books (confirmed)
@@ -273,9 +297,9 @@ def fetchItem(itemurl, record):
 	except:
 		record['Name'] = ''
 
-	# 4. Star Ratings
-	# 7. Reviews (Recent 100): review Text, rating and timestamp, Reviewer
-	# 8. Reviewer Info: Name, Rating, rating, location
+	# Task 4. Star Ratings
+	# Task 7. Reviews (Recent 100): review Text, rating and timestamp, Reviewer
+	# Task 8. Reviewer Info: Name, Rating, rating, location
 	reviews = { 'MaxNumReviews' : MaxNumReviews }
 
 	try:
@@ -294,7 +318,7 @@ def fetchItem(itemurl, record):
 
 	return True
 
-def amazoncrawl(startUrl, keywords):
+def amazoncrawl(startUrl, keywords, path):
 	"""
 		This function crawls the item listing page returned by the search 
 		it parses the page to extract the each item's URL and calls fetchItem()
@@ -336,6 +360,11 @@ def amazoncrawl(startUrl, keywords):
 				valid = fetchItem(itemurl, record) # pull the item
 				if valid:
 					collected += 1
+					h = hashlib.md5()
+					h.update(itemurl)
+					print h.hexdigest()
+					with open(path + '/' + h.hexdigest() + '.json', 'w') as f:
+						json.dump(record, f)
 
 			# not enough, we are greedy. Go to next listing page
 			nextPage = soup.find('a', class_=nextlocator_class)
@@ -344,22 +373,38 @@ def amazoncrawl(startUrl, keywords):
 			else:
 				pageurl = None 
 
-def main():
+def main(config):
 
-	with open('test.json') as f:
-
-		categories = json.load(f)
-	
+	with open(config) as f:
+		try:
+			categories = json.load(f)
+		except:
+			print "Error: invalid JSON file"
+			return
+			
+		print "Yes, Works"
+		print categories
+		return 
 		for type in categories:
 
 			print "Crawling [", type, "] ..."
 			info = categories[type]
 
-			if info['url'] == '':  # FIXME : let's skip it for now
+			# we could use search to find the url of product list for 
+			# each category. For now, let's skip it
+			if info['url'] == '': 
 				print "Fetching url" 
 				continue;
 
-			amazoncrawl(info['url'], info['keywords'])
+			if not os.path.exists(type): 
+				os.makedirs(type)
+			else:
+				h = hashlib.md5()
+				h.update(info['url'])
+				# if already exits, then just skip
+				if os.path.exists(type + '/' + h + '.json'):
+					continue;
+			amazoncrawl(info['url'], info['keywords'], type)
 	
 def testReviewer():
 	Author = {}
@@ -369,7 +414,9 @@ def testReviewer():
 
 def testItem():
 	record = {}
-	fetchItem('http://www.amazon.com/FIFA-15-PlayStation-4/dp/B00KPY1GJA/ref=sr_1_1?s=videogames&ie=UTF8&qid=1429065279&sr=1-1&keywords=FIFA', record)
+	#fetchItem('http://www.amazon.com/Illustrated-Guide-Home-Chemistry-Experiments/dp/0596514921', record)
+	fetchItem('http://www.amazon.com/Path-Way-Knowledg-Containing-Principles-Geometrie-ebook/dp/B004TPZGEC', record) # kindle
+	#fetchItem('http://www.amazon.com/FIFA-15-PlayStation-4/dp/B00KPY1GJA/ref=sr_1_1?s=videogames&ie=UTF8&qid=1429065279&sr=1-1&keywords=FIFA', record)
 	print(record)
 
 def test():
@@ -383,7 +430,15 @@ def test():
 	#fetchItem('http://www.amazon.com/Path-Way-Knowledg-Containing-Principles-Geometrie-ebook/dp/B004TPZGEC') # kindle
 
 if __name__ == "__main__":
+	
+	if len(sys.argv) < 2:
+		print "Usage: python crawl.py [books.json]"
+		exit(0)
+	
+	if not os.path.exists(sys.argv[1]): 
+		print "Error: file [" + sys.argv[1] + "] does not exist"
+		exit(0)
 
-	#main()
-	test()
+	main(sys.argv[1])
+	#test()
 	
